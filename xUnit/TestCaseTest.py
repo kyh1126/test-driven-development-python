4969c95e5625a36383212dedd8048a1be9362f31
kyh1126 <kyh901126@nate.com>
2021-05-24 16:38:49 +0900

    [19장. 테이블 차리기] 작은 테스트를 추가한다. (주기 1)

--- xUnit/TestCaseTest.py
+++ xUnit/TestCaseTest.py
@@ -9,5 +9,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert test.wasRun
 
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetUp
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
