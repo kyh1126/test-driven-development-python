90b99d6cf93c4f392b7a44cd5e15854e5a352a93
kyh1126 <kyh901126@nate.com>
2021-05-24 15:24:50 +0900

    [18장. xUnit으로 가는 첫걸음] 파이썬은 클래스명이나 메서드명을 함수처럼 다룰 수 있다.

--- xUnit/WasRun.py
+++ xUnit/WasRun.py
@@ -1,9 +1,12 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def testMethod(self):
         self.wasRun = 1
 
     def run(self):
-        self.testMethod()
+        # self.testMethod()
+        method = getattr(self, self.name)
+        method()
