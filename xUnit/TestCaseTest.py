cdf5a50fb602607656b17a04cfa3dc351d06ad27
kyh1126 <kyh901126@nate.com>
2021-05-24 17:11:25 +0900

    [19장. 테이블 차리기] 먼저 setUp()을 호출하게 해, 테스트를 단순화 한다.

--- xUnit/TestCaseTest.py
+++ xUnit/TestCaseTest.py
@@ -3,16 +3,17 @@ from xUnit.WasRun import WasRun
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        # assert not test.wasRun
+        self.test.run()
+        assert self.test.wasRun
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasSetUp
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("testRunning").run()
--- xUnit/WasRun.py
+++ xUnit/WasRun.py
@@ -3,11 +3,11 @@ from xUnit.TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
