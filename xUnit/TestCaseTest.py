6ef550c6ede3ced1f5bd968c2619c382d98fddec
kyh1126 <kyh901126@nate.com>
2021-05-24 17:44:34 +0900

    [20장. 뒷정리하기] testSetUp() 은 이제 testRunning() 과 testSetUp() 두 역할을 다 한다. - testTemplateMethod 로 변경

--- xUnit/TestCaseTest.py
+++ xUnit/TestCaseTest.py
@@ -3,18 +3,19 @@ from xUnit.WasRun import WasRun
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    # def setUp(self):
+    #     self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        # assert not test.wasRun
-        self.test.run()
-        assert self.test.wasRun
+    # def testRunning(self):
+    #     self.test.run()
+    #     assert self.test.wasRun
 
-    def testSetUp(self):
-        self.test.run()
-        assert "setUp testMethod " == self.test.log
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod " == test.log
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+# TestCaseTest("testRunning").run()
+# TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
