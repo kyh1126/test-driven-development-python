e68747cb9b8919778af53bce1ebec7e8d50e0074
kyh1126 <kyh901126@nate.com>
2021-05-24 19:16:47 +0900

    [21장. 셈하기] 모든 테스트를 실행하고, 실패하는 것을 확인한다, 코드에 변화를 준다. - summary() 구현 실체화, 모든 테스트를 실행하고, 성공하는 것을 확인한다. (주기 2, 3, 4)

--- xUnit/TestCase.py
+++ xUnit/TestCase.py
@@ -1,12 +1,19 @@
+from xUnit.TestResult import TestResult
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
+
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def setUp(self):
         pass
